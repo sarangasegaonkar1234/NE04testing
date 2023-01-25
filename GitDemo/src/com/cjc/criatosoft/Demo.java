6959d5fd63a3a47a3f091517d2f373ea9a971a61
sarangasegaonkar1234 <123531419+sarangasegaonkar1234@users.noreply.github.com>
2023-01-25 18:06:40 +0530

    Update Demo.java

--- GitDemo/src/com/cjc/criatosoft/Demo.java
+++ GitDemo/src/com/cjc/criatosoft/Demo.java
@@ -1,7 +1,9 @@
 package com.cjc.criatosoft;
 
 public class Demo {
-
+	public void m1()
+	{
+	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("dcxgcvbhnbjnm,.,");
